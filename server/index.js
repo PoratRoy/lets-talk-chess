9fe9b4f6c0d60a218f77f8bf8e5772bec79498b5
Roy Porat <porat.roy@gmail.com>
2021-08-17 11:33:18 +0300

    .env

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use( (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://lets-talk-chess.netlify.app");
+    res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();

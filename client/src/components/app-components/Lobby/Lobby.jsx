93bea5bb4099846bee854a5b04f21560854efb95
Roy Porat <porat.roy@gmail.com>
2021-08-27 23:27:25 +0300

    loading

--- client/src/components/app-components/Lobby/Lobby.jsx
+++ client/src/components/app-components/Lobby/Lobby.jsx
@@ -3,7 +3,7 @@ import Axios from "axios";
 import { useHistory } from "react-router-dom";
 import UserContext from "../../../context/UserContext";
 import HasErrorContext from "../../../context/HasErrorContext";
-import {SocketContext} from "../../../context/SocketContext"
+import { SocketContext } from "../../../context/SocketContext";
 import UserLink from "./UserLink/UserLink";
 import { Row, NavBar } from "../../UIKit";
 import "./Lobby.css";
@@ -13,7 +13,8 @@ const Lobby = () => {
   const [users, setUsers] = useState([]);
   const [activeUsers, setActiveUsers] = useState([]);
   const [gameAlertIds, setGameAlertIds] = useState([]);
-  
+  const [loading, setLoading] = useState(true);
+
   const { userData, setUserData } = useContext(UserContext);
   const { setHasError } = useContext(HasErrorContext);
   const { socket } = useContext(SocketContext);
@@ -32,46 +33,48 @@ const Lobby = () => {
     }
   }, [history, setUserData, userData.user]);
 
-  useEffect(()=>{
+  useEffect(() => {
     //Listtning to sockets alerts
-    
-    socket.on('receiveAllActiveUsers',(data)=>{
-      setActiveUsers(data)
-    })
 
-    socket.on('getNewRegisterUser',(data)=>{
+    socket.on("receiveAllActiveUsers", (data) => {
+      setActiveUsers(data);
+    });
+
+    socket.on("getNewRegisterUser", (data) => {
       data && addNewUser(data);
-    })
+    });
 
-    socket.on('alertAboutNewGame',(data)=>{
-      setGameAlertIds(prev => [...prev,data.senderId])
-    })
-
-  })
+    socket.on("alertAboutNewGame", (data) => {
+      setGameAlertIds((prev) => [...prev, data.senderId]);
+    });
+  });
 
   //Add the new user to the users array so he will show up on the list
   const addNewUser = async (userId) => {
-    try{
+    try {
       const result = await Axios.get(
         `${process.env.REACT_APP_SERVER_URL}private/${userId}`,
         { headers: { Authorization: localStorage.getItem("auth-Token") } }
       );
       setUsers((prev) => [...prev, result.data]);
-    } catch(error){
+    } catch (error) {
       setHasError(error);
     }
   };
-
 
   //Controlle the users list
   useEffect(() => {
     const getAllUsers = async () => {
       //Get all the users how are not the current user
       try {
+        setLoading(true);
+
         const result = await Axios.get(
           `${process.env.REACT_APP_SERVER_URL}private/all`,
           { headers: { Authorization: localStorage.getItem("auth-Token") } }
         );
+
+        setLoading(false);
 
         let filterUsers = result.data.filter((u) => {
           return u._id !== userData.user?._id;
@@ -84,22 +87,29 @@ const Lobby = () => {
     };
     getAllUsers();
 
-    socket.emit('getAllActiveUsers')
-
-  }, [setHasError, userData.user?._id, socket]);
+    socket.emit("getAllActiveUsers");
+  }, [setHasError, userData.user?._id, socket, setLoading]);
 
   return (
     <>
       <section>
-        <NavBar isLobby={true} setUserData={setUserData}/>
+        <NavBar isLobby={true} setUserData={setUserData} />
       </section>
+
       <div className="lobby-continer">
         <Row>
           <header className="lobby-header">All Your Friends</header>
 
+          {loading ? <img src={require('../../../assets/app-images/loading.gif').default} aly="loading..." className="lobby-loading" /> : null}
+
           <ul className="lobby-groups">
             {users.map((u) => (
-              <UserLink key={u._id} user={u} isActive={activeUsers.includes(u._id)} isGameAlert={gameAlertIds.includes(u._id)}/>
+              <UserLink
+                key={u._id}
+                user={u}
+                isActive={activeUsers.includes(u._id)}
+                isGameAlert={gameAlertIds.includes(u._id)}
+              />
             ))}
           </ul>
         </Row>
